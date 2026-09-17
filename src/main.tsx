1e02a9e95bceb81fa6d795860191bde31e11eb24
xturn2u <73440088+xturn2u@users.noreply.github.com>
2026-09-17 15:08:22 +0200

    style: load production workflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './version-flows.css';
+import './workflow-v2.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);

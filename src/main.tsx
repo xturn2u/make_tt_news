158e0d480ab20cdc387069ff7c3a257ebc7e330a
xturn2u <73440088+xturn2u@users.noreply.github.com>
2026-09-17 15:31:19 +0200

    style: load studio and headline overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 import './styles.css';
 import './version-flows.css';
 import './workflow-v2.css';
+import './studio-overrides.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
